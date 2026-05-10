8bb1763f752628e084d64eb0c32bb24902090232
Condorito666 <domingo123@gmail.com>
2026-05-09 20:29:21 -0400

    setup proyecto

---
+++ inventory-software/app/(oficina)/dashboard/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function layout({ children }) {
+    return (
+        <div className='bg-gray-300'>layout</div>
+    )
+}
+
+export default layout
---
+++ inventory-software/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" min-h-screen flex justify-center items-center">
+      <h1>Inventory managment software</h1>
+    </div>
+  );
+}
